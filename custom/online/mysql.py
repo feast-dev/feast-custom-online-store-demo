f41ae02ab159bfb3d338514d4fe1e9c5038b408d
Achal Shah <achals@gmail.com>
2021-08-20 09:58:22 -0700

    Initial commit for mysql-backed custom online store example
    Signed-off-by: Achal Shah <achals@gmail.com>

---
+++ custom/online/mysql.py
@@ -0,0 +1,194 @@
+from datetime import datetime
+from typing import Sequence, Union, List, Optional, Tuple, Dict, Callable, Any
+
+import pytz
+from feast import RepoConfig, FeatureTable, FeatureView, Entity
+from feast.infra.key_encoding_utils import serialize_entity_key
+from feast.infra.online_stores.online_store import OnlineStore
+from feast.protos.feast.types.EntityKey_pb2 import EntityKey as EntityKeyProto
+from feast.protos.feast.types.Value_pb2 import Value as ValueProto
+
+
+import mysql.connector
+from feast.repo_config import FeastConfigBaseModel
+from pydantic import StrictStr
+from pydantic.typing import Literal
+
+
+class MySQLOnlineStoreConfig(FeastConfigBaseModel):
+    type: Literal["mysql", "custom.online.mysql.MySQLOnlineStore"] = "custom.online.mysql.MySQLOnlineStore"
+
+    host: Optional[StrictStr] = None
+    user: Optional[StrictStr] = None
+    password: Optional[StrictStr] = None
+    database: Optional[StrictStr] = None
+
+
+class MySQLOnlineStore(OnlineStore):
+
+    _conn = None
+    _cur = None
+
+    def _get_conn(self, config: RepoConfig):
+
+        online_store_config = config.online_store
+        assert isinstance(online_store_config, MySQLOnlineStoreConfig)
+
+        if not self._conn:
+            self._conn = mysql.connector.connect(
+                host=online_store_config.host or "127.0.0.1",
+                user=online_store_config.user or "root",
+                password=online_store_config.password,
+                database=online_store_config.database or "feast",
+                autocommit=True
+            )
+        return self._conn
+
+    def online_write_batch(
+            self,
+            config: RepoConfig,
+            table: Union[FeatureTable, FeatureView],
+            data: List[
+                Tuple[EntityKeyProto, Dict[str, ValueProto], datetime, Optional[datetime]]
+            ],
+            progress: Optional[Callable[[int], Any]],
+    ) -> None:
+
+        conn = self._get_conn(config)
+        cur = conn.cursor(buffered=True)
+
+        project = config.project
+
+        for entity_key, values, timestamp, created_ts in data:
+            entity_key_bin = serialize_entity_key(entity_key).hex()
+            timestamp = _to_naive_utc(timestamp)
+            if created_ts is not None:
+                created_ts = _to_naive_utc(created_ts)
+
+            for feature_name, val in values.items():
+                self.write_to_table(created_ts, cur, entity_key_bin, feature_name, project, table, timestamp, val)
+            self._conn.commit()
+            if progress:
+                progress(1)
+
+    @staticmethod
+    def write_to_table(created_ts, cur, entity_key_bin, feature_name, project, table, timestamp, val):
+        cur.execute(
+            f"""
+                        UPDATE {_table_id(project, table)}
+                        SET value = %s, event_ts = %s, created_ts = %s
+                        WHERE (entity_key = %s AND feature_name = %s)
+                    """,
+            (
+                # SET
+                val.SerializeToString(),
+                timestamp,
+                created_ts,
+                # WHERE
+                entity_key_bin,
+                feature_name,
+            ),
+        )
+        cur.execute(
+            f"""INSERT INTO {_table_id(project, table)}
+                        (entity_key, feature_name, value, event_ts, created_ts)
+                        VALUES (%s, %s, %s, %s, %s)""",
+            (
+                entity_key_bin,
+                feature_name,
+                val.SerializeToString(),
+                timestamp,
+                created_ts,
+            ),
+        )
+
+    def online_read(
+            self,
+            config: RepoConfig,
+            table: Union[FeatureTable, FeatureView],
+            entity_keys: List[EntityKeyProto],
+            requested_features: Optional[List[str]] = None,
+    ) -> List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]]:
+        conn = self._get_conn(config)
+        cur = conn.cursor(buffered=True)
+
+        result: List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]] = []
+
+        project = config.project
+        for entity_key in entity_keys:
+            entity_key_bin = serialize_entity_key(entity_key).hex()
+            print(f"entity_key_bin: {entity_key_bin}")
+
+            cur.execute(
+                f"SELECT feature_name, value, event_ts FROM {_table_id(project, table)} WHERE entity_key = %s",
+                (entity_key_bin,),
+            )
+
+            res = {}
+            res_ts = None
+            for feature_name, val_bin, ts in cur.fetchall():
+                val = ValueProto()
+                val.ParseFromString(val_bin)
+                res[feature_name] = val
+                res_ts = ts
+
+            if not res:
+                result.append((None, None))
+            else:
+                result.append((res_ts, res))
+        return result
+
+    def update(
+            self,
+            config: RepoConfig,
+            tables_to_delete: Sequence[Union[FeatureTable, FeatureView]],
+            tables_to_keep: Sequence[Union[FeatureTable, FeatureView]],
+            entities_to_delete: Sequence[Entity],
+            entities_to_keep: Sequence[Entity],
+            partial: bool,
+    ):
+        conn = self._get_conn(config)
+        cur = conn.cursor(buffered=True)
+
+        project = config.project
+
+        for table in tables_to_keep:
+            cur.execute(
+                f"CREATE TABLE IF NOT EXISTS {_table_id(project, table)} (entity_key VARCHAR(512), feature_name VARCHAR(256), value BLOB, event_ts timestamp, created_ts timestamp,  PRIMARY KEY(entity_key, feature_name))"
+            )
+            cur.execute(
+                f"CREATE INDEX {_table_id(project, table)}_ek ON {_table_id(project, table)} (entity_key);"
+            )
+
+        for table in tables_to_delete:
+            cur.execute(
+                f"DROP INDEX {_table_id(project, table)}_ek ON {_table_id(project, table)};"
+            )
+            cur.execute(f"DROP TABLE IF EXISTS {_table_id(project, table)}")
+
+    def teardown(
+            self,
+            config: RepoConfig,
+            tables: Sequence[Union[FeatureTable, FeatureView]],
+            entities: Sequence[Entity],
+    ):
+        conn = self._get_conn(config)
+        cur = conn.cursor(buffered=True)
+        project = config.project
+
+        for table in tables:
+            cur.execute(
+                f"DROP INDEX {_table_id(project, table)}_ek ON {_table_id(project, table)};"
+            )
+            cur.execute(f"DROP TABLE IF EXISTS {_table_id(project, table)}")
+
+
+def _table_id(project: str, table: Union[FeatureTable, FeatureView]) -> str:
+    return f"{project}_{table.name}"
+
+
+def _to_naive_utc(ts: datetime):
+    if ts.tzinfo is None:
+        return ts
+    else:
+        return ts.astimezone(pytz.utc).replace(tzinfo=None)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+NAME = "feast-custom-stores-demo"
+DESCRIPTION = "Python SDK for Feast"
+URL = "https://github.com/feast-dev/feast"
+AUTHOR = "Feast"
+REQUIRES_PYTHON = ">=3.7.0"
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=("tests", "example.py")),
+    install_requires=[
+        "mysql-connector-python",
+        "feast>=0.12"
+    ],
+    license="Apache",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
